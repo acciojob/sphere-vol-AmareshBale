fb35616caf1b687a7504e010ea6818de83cf96de
AmareshBale <137062501+AmareshBale@users.noreply.github.com>
2024-01-22 13:51:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ function volume_sphere() {
 	let radius = parseFloat(document.getElementById("radius").value);
 
 	if(isNaN(radius) || radius<0) {
+		document.getElementById("volume").value = ''; 
 		return false;
+		
 	}
 
 	let volume = (4/3)*Math.PI*Math.pow(radius,3);
