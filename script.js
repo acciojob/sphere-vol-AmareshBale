c9458eaaeb629ce30277f9813e2c0991c64a61f0
AmareshBale <137062501+AmareshBale@users.noreply.github.com>
2024-01-22 13:42:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function volume_sphere() {
 	}
 
 	let volume = (4/3)*Math.PI*Math.pow(radius,3);
-	document.getElementById("volume").value = volume.toFixed(4)
+	document.getElementById("volume").value = volume.toString(4)
 	return false;
 	
   
